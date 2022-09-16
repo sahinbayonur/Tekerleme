b29dab0dcfb11cd43d2a4ef00a1da750707fbdf2
sahinbayonur <sahinbayonur@gmail.com>
2022-09-16 15:30:01 +0300

    Initial commit

--- app/src/main/java/com/sahinbay/tekerleme/MainActivity.java
+++ app/src/main/java/com/sahinbay/tekerleme/MainActivity.java
@@ -11,6 +11,7 @@ import android.speech.RecognizerIntent;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -21,13 +22,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView txtSpeechInput, txtTekerleme;
+    private TextView answer, txtTekerleme;
     private ImageButton btnSpeak;
     private Button btnContinue;
     private String gelen_tekerleme;
+    private TextView txtSoylenen;
     private final int REQ_CODE_SPEECH_INPUT = 100;
+    int indexValue = 0;
+    int indexCompareValue = 0;
 
-    String[] list = {"Dal sarkar kartal kalkar kartal kalkar dal sarkar", "Üç tunç tas kayısı hoşafı", "Adem madene gitmiş Adem madende badem yemiş , madem ki Adem madende badem yemiş , niye bize getirmemiş ."};
+    String[] list = {"ali veli ali selami ali"};
     int rastgele = (int) (Math.random() * list.length);
 
     @Override
@@ -36,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtTekerleme = (TextView) findViewById(R.id.txtTekerleme);
-        txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
+        answer = (TextView) findViewById(R.id.txtSpeechInput);
+        txtSoylenen = (TextView) findViewById(R.id.kullanici_konusmasi);
         btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
         btnContinue = (Button) findViewById(R.id.devam);
 
@@ -45,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnContinue.setEnabled(false);
         // btnContinue.setClickable(false);
-        btnContinue.setAlpha(0.5f);
+        // btnContinue.setAlpha(0.5f);
         btnContinue.getBackground().setAlpha(50);
 
         btnSpeak.setOnClickListener(new View.OnClickListener() {
@@ -61,14 +66,14 @@ public class MainActivity extends AppCompatActivity {
                 btnContinue.setEnabled(false);
                 btnContinue.setAlpha(0.5f);
 
-                txtSpeechInput.setText("...");
+                answer.setText("...");
+                answer.setTextColor(Color.WHITE);
 
                 int rastgele = (int) (Math.random() * list.length);
                 gelen_tekerleme = list[rastgele];
                 txtTekerleme.setText(gelen_tekerleme);
             }
         });
-
     }
 
     private void promptSpeechInput() {
@@ -94,50 +99,83 @@ public class MainActivity extends AppCompatActivity {
             case REQ_CODE_SPEECH_INPUT: {
                 if (resultCode == RESULT_OK && null != data) {
 
-
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     // txtSpeechInput.setText(result.get(0));
 
                     ArrayList<String> mList = new ArrayList<>();
+                    ArrayList<String> artik = new ArrayList<>();
+                    ArrayList<Integer> yeniOnurList = new ArrayList<>();
 
                     // if (gelen_tekerleme.equalsIgnoreCase(result.get(0))) {
                     //tekerleme string
                     String tekelerme = gelen_tekerleme;
-                    //Words to split
-                    String[] words = tekelerme.split(" ");
-                    SpannableString tekelerme_str = new SpannableString(tekelerme);
-                    //main string
-                    String soylenen = result.get(0);
-                    //split strings by space
-                    String[] splittedWords = soylenen.split(" ");
-                    SpannableString str = new SpannableString(soylenen);
 
-                    //Check the matching words
-                    for (int i = 0; i < words.length; i++) {
-                        for (int j = 0; j < splittedWords.length; j++) {
-                            if (words[i].equalsIgnoreCase(splittedWords[j])) {
-                                mList.add(words[i]);
-                            }
+                    // Words to split
+                    String[] tekerleme_words = tekelerme.split(" ");
+
+                    // main string
+                    String user_soylenen = result.get(0);
+
+                    // split strings by space
+                    String[] soylenen_words = user_soylenen.split(" ");
+                    SpannableString soylenen_str = new SpannableString(user_soylenen);
+
+                    // Check the matching words
+                    // for (int i = 0; i < tekerleme_words.length; i++) {
+
+                    for (int j = 0; j < soylenen_words.length; j++) {
+
+                        if (tekerleme_words[j].equalsIgnoreCase(soylenen_words[j])) {
+                            mList.add(tekerleme_words[j]);
+                            Log.e("Check the matching words", "" + mList.size());
+                            // Toast.makeText(getApplicationContext(), "eşleşti" + mList, Toast.LENGTH_SHORT).show();
+                        } else {
+                            artik.add(tekerleme_words[j]);
+                            Log.e("HAYIR", "" + artik.size());
                         }
                     }
+                    // listeyi görmek için log ekledim.
+                    Log.e("bu liste", "" + mList);
+                    // }
 
-                    //make the words bold
+                    SpannableString tekelerme_str = new SpannableString(tekelerme);
+
+                    // make the words bold
                     for (int k = 0; k < mList.size(); k++) {
-                        int val = tekelerme.indexOf(mList.get(k));
+                        indexValue = tekelerme.indexOf(mList.get(k));
                         // str.setSpan(new StyleSpan(Typeface.BOLD), val, val + mList.get(k).length(),
-                        tekelerme_str.setSpan(new ForegroundColorSpan(Color.GREEN), val, val + mList.get(k).length(),
+                        if (indexValue >= indexCompareValue) {
+                            indexCompareValue = indexValue;
+                            Log.e("tab", "" + indexCompareValue + ", bak : " + k);
+                        } else {
+                            indexValue = tekelerme.lastIndexOf(mList.get(k));
+                            indexCompareValue = indexValue;
+                            Log.e("yap", "" + indexCompareValue + ", bak : " + k);
+                        }
+
+                        yeniOnurList.add(indexCompareValue);
+
+                        Log.e("deger", "" + indexValue);
+                    }
+                    Log.e("tablomun listesi", "" + yeniOnurList);
+
+                    for (int z = 0; z < mList.size(); z++) {
+                        tekelerme_str.setSpan(new ForegroundColorSpan(Color.GREEN), yeniOnurList.get(z), yeniOnurList.get(z) + mList.get(z).length(),
                                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
                     }
 
-                    //set text
-                    TextView answer = (TextView) findViewById(R.id.txtSpeechInput);
-                    answer.setTextColor(Color.RED);
+                    // set text
                     answer.setText(tekelerme_str);
+                    answer.setTextColor(Color.RED);
 
                     // txtTekerleme.setText(soylenen);
+                    txtSoylenen.setText(user_soylenen);
                     btnContinue.setEnabled(true);
                     btnContinue.setAlpha(1.0f);
-                    Toast.makeText(getApplicationContext(), "devam aktif", Toast.LENGTH_SHORT).show();
+
+                    indexValue = 0;
+                    indexCompareValue = 0;
+                    // Toast.makeText(getApplicationContext(), "devam aktif", Toast.LENGTH_SHORT).show();
                     //  }
                 }
                 break;
